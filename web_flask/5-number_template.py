7ad5ed5d41e73278486e00db92a9a3c023928f86
Praise Obah <obahsomto@gmail.com>
2024-02-18 05:45:54 -0800

    changes

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,7 +37,13 @@ def pythoniscool(text='is cool'):
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """display a HTML page only if n is an integer"""
+    return "{:d} is a number".format(n)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_render(n):
+    """display a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
